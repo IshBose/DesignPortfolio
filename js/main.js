fb9a491caf6731b5579fae76622a5d8f57cfd357
Ishaan Bose <ibose@ucsd.edu>
2023-09-06 21:44:23 -0700

    Adding in scroll magic and pinning element

--- js/main.js
+++ js/main.js
@@ -99,5 +99,21 @@ let section = document.querySelectorAll('section');
 
 // JavaScript for handling the sticky behavior
 
+// init ScrollMagic Controller
+var controller = new ScrollMagic.Controller();
+
+// Scene Handler
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#pinned-trigger1", // point of execution
+  duration: $(window).height() - 50, // pin element for the window height - 1
+  triggerHook: 0, // don't trigger until #pinned-trigger1 hits the top of the viewport
+  reverse: true // allows the effect to trigger when scrolled in the reverse direction
+})
+.setPin("#pinned-element1") // the element we want to pin
+.addTo(controller);
+
+
+
+
 
   
