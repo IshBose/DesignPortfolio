27fc01c0b0d1be9eba61dc367e268cac9b3f81c4
Ishaan Bose <ibose@ucsd.edu>
2024-06-19 21:40:10 -0700

    index Change and adding new projects

--- js/main.js
+++ js/main.js
@@ -56,5 +56,19 @@ let section = document.querySelectorAll('section');
         };
 
 
-        /* Scroll Swap images in case study  */
-         
+      
+
+        var coll = document.getElementsByClassName("collapsible");
+        var i;
+
+        for (i = 0; i < coll.length; i++) {
+          coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+              content.style.display = "none";
+            } else {
+              content.style.display = "block";
+            }
+          });
+        }
